a211cdc08d3c62f21ad317925f352288e9312bf5
Mohammed Salman <msal4@outlook.com>
2022-11-01 00:01:31 +0100

    Updae setup.sh to link nvim too

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
 ln -nfs $HOME/dotfiles/vim $HOME/.vim
+ln -nfs $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
