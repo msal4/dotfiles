211e206de48a9d039cd68bbadbab4f85c06b2064
Mohammed Salman <msal4@outlook.com>
2024-12-25 12:02:07 +0300

    Add mkdir

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 ln -nfs $HOME/dotfiles/vim $HOME/.vim
+mkdir -p $HOME/dotfiles/.config/nvim
 ln -nfs $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
