f8e3aa626cc0affa686c0c29e06c866ae069b417
Salman Sarray <msal4@outlook.com>
2025-07-26 20:12:45 +0200

    Update script

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ ln -nfs $(pwd)/vim $HOME/.vim
 mkdir -p .config/nvim
 ln -nfs $(pwd)/nvim $HOME/.config/nvim
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/dotfiles/zshrc $HOME/.zshrc
+ln -s $(pwd)/dotfiles/zprofile $HOME/.zprofile
