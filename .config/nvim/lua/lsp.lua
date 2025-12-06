70b36f7645ee1f2de820f296e9b1c432372520f5
Salman Sarray <msal4@outlook.com>
2025-12-06 19:19:05 +0100

    Update stuff

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,14 +1,5 @@
 -- Lua config for lspconfig
 
-vim.lsp.config("denols", {
-	cmd = { "deno", "lsp" },
-	filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-	root_markers = {"deno.json", "deno.jsonc"},
-	handlers = {
-		["textDocument/publishDiagnostics"] = function() end,
-	},
-})
-
 vim.diagnostic.enable(false, …)
 vim.lsp.enable('gopls')
 vim.lsp.enable('dartls')
